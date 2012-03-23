c8f8c071d802996ba958ab6d4e0dc6985bf033d9
Alexandr <Gsashko@mail.ru>
2012-03-23 11:43:35 +0200

    encrypting password

--- lib/model.rb
+++ lib/model.rb
@@ -1,6 +1,8 @@
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/lib/reporter.db")
 
 class User
+  require 'bcrypt'
+
     include DataMapper::Resource
 
     property :id,           Serial
@@ -8,12 +10,23 @@ class User
     property :email,        String
     property :login,        String
     property :position,        String
-    property :password,        String
+    property :password_hash,        String
+    property :password_salt,        String
     property :created_at,   DateTime
+    attr_accessor :password
+
+    before :save, :encrypt_password
+
+  def encrypt_password
+    if password
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 
   def self.authenticate(login, password)
     user = User.first(:login => login)
-    if user && user.password == password
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
       nil
@@ -22,5 +35,16 @@ class User
   end
 
 end
+
+
+#class Project
+#    include DataMapper::Resource
+#
+#    property :id,           Serial
+#    property :name,        String
+#
+#
+#
+#end
 
   DataMapper.auto_upgrade!
